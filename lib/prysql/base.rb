f53a09fdf983882215016e8fc9acbc00ae6bea56
Josh Aronson <jparonson@gmail.com>
2012-05-01 18:52:17 -0500

    Reformat CommandSet definitions

--- lib/prysql/base.rb
+++ lib/prysql/base.rb
@@ -1,12 +1,14 @@
 class Prysql
-  SQL_PROMPT = [
-    proc{|*args| "prysql #{Prysql.username}@#{Prysql.host} [#{Prysql.database}] $ " },
-    proc{|*args| "prysql #{Prysql.username}@#{Prysql.host} [#{Prysql.database}] * " }
-  ]
-
   SQL_COMMANDS = CodeRay::Scanners::SQL::COMMANDS + [ 'describe' ]
 
-  GlobalCommandSet = Pry::CommandSet.new do
+  CommandSet = Pry::CommandSet.new do
+    ASSIGNMENT_MATCHER = /([a-zA-Z_]*)\s+(=)\s+(prysql)(.*)/
+
+    SQL_PROMPT = [
+      proc{|*args| "prysql #{Prysql.username}@#{Prysql.host} [#{Prysql.database}] $ " },
+      proc{|*args| "prysql #{Prysql.username}@#{Prysql.host} [#{Prysql.database}] * " }
+    ]
+
     create_command 'prysql' do |cmd|
       group 'Prysql'
       description 'Pry mysql interface.'
@@ -23,90 +25,89 @@ Examples:
   `local_variable = prysql SELECT * FROM zip_codes`
 
 Available commands:
-#{Prysql::Interface.formatted_commands}
+#{Prysql::Commands.formatted_commands}
 BANNER
 
       def options(opt)
-        opt.on :v, :vertical, 'Print output in vertical format', :optional => true
+        opt.on :v, :vertical, 'Print output in vertical format',       :optional => true
+        opt.on :c, :clear,    'Clear the current input statement',     :optional => true
+        opt.on :s, :search,   'Highlight given string',                :optional => true
       end
 
       def process
-        interface = Prysql.interface
-        cmd       = args.shift
-        cmd_opts  = {}
+        return nil if opts.present?(:clear)
 
-        return interface.help if cmd.nil?
+        if arg_string =~ ASSIGNMENT_MATCHER
+          assign_local_result
+        else
+          print_result
+        end
+      end
+
+      def print_result
+        interface     = Prysql.interface
+        cmd, cmd_opts = args.shift, {}
 
         cmd_opts[:vertical] = true if opts.present?(:vertical)
 
         if interface.has_command?(cmd)
           interface.execute_command(cmd, args, cmd_opts)
         elsif interface.command_is_sql?(cmd)
-          args.unshift(cmd)
-          interface.execute(args, cmd_opts)
+          interface.execute(args.unshift(cmd), cmd_opts)
         else
           interface.help(cmd)
         end
       end
+
+      def assign_local_result
+        var = args.shift
+        sql = args.delete_if{|a| a == '=' || a == 'prysql'}.join(' ')
+
+        result = Prysql.interface.execute(sql, :return => true)
+        _pry_.inject_local(var, result, _pry_.binding_stack.last)
+        result
+      end
     end
+
+    alias_command(ASSIGNMENT_MATCHER, 'prysql', {
+      :listing     => '= prysql',
+      :desc        => 'Assign prysql output to a local variable'
+    })
 
     create_command 'prysql-mode' do
       group 'Prysql'
       description 'Toggle prysql shell mode.'
 
       def process(*args)
-        instance_eval &Prysql.toggle_shell_mode_proc
+        case _pry_.prompt
+        when Prysql::SQL_PROMPT
+          _pry_.pop_prompt
+          _pry_.custom_completions = Pry::DEFAULT_CUSTOM_COMPLETIONS
+          remove_prysql_commands
+        else
+          _pry_.push_prompt Prysql::SQL_PROMPT
+          _pry_.custom_completions = proc{ Prysql.interface.completions }
+          add_prysql_commands
+        end
+      end
+
+      def add_prysql_commands
+        Prysql::Commands.available_commands.each do |cmd|
+          Pry::Commands.alias_command cmd, "prysql #{cmd}"
+        end
+
+        SQL_COMMANDS.each do |cmd|
+          Pry::Commands.alias_command /^(#{cmd}.*)/, 'prysql'
+        end
+      end
+
+      def remove_prysql_commands
+        Pry::Commands.delete *Prysql::Commands.available_commands.map{|cmd| cmd }
+        Pry::Commands.delete *SQL_COMMANDS.map{|cmd| /^(#{cmd}.*)/ }
       end
     end
 
     alias_command 'sql-mode', 'prysql-mode'
-
-    create_command(/([a-zA-Z_]+)\s+=\s+prysql(.*)/, :listing => "= prysql ...") do |var, sql|
-      group 'Prysql'
-      description 'Assign prysql output to a local variable.'
-
-      def process(var, sql)
-        instance_eval &Prysql.query_assignment_proc(var, sql)
-      end
-    end
-  end
-
-  SQLModeCommandSet = Pry::CommandSet.new do
-    SQL_COMMANDS.each do |sql_command|
-      create_command(/^(#{sql_command})(.*)/i, :listing => "#{sql_command.upcase} ...") do
-        group 'Prysql shell mode'
-        description "Perform a SQL #{sql_command.upcase} statement."
-
-        def options(opt)
-          opt.on :v, :vertical, 'Print output in vertical format', :optional => true
-        end
-
-        # TODO: Get options to work here!
-        def process
-          Prysql.interface.execute(captures, cmd_opts)
-        end
-      end
-
-      create_command(/^(use)(.*)/i, :listing => 'USE ...') do |cmd, db|
-        group 'Prysql shell mode'
-        description 'Switch databases.'
-
-        def process(cmd, db)
-          Prysql.interface.use(db)
-          # Reset shell mode
-          2.times{ instance_eval &Prysql.toggle_shell_mode_proc }
-        end
-      end
-
-      create_command(/([a-zA-Z_]+)\s+=\s+(#{sql_command}.*)/i, :listing => "= #{sql_command.upcase} ...") do |var, sql|
-        group 'Prysql shell mode'
-        description "Assign the output of a SQL #{sql_command} statement to a local variable."
-
-        def process(var, sql)
-          instance_eval &Prysql.query_assignment_proc(var, sql)
-        end
-      end
-    end
   end
 
   class << self
@@ -116,29 +117,6 @@ BANNER
 
     def interface
       @@interface ||= Prysql::Interface.new
-    end
-
-    def toggle_shell_mode_proc
-      proc do
-        case _pry_.prompt
-        when Prysql::SQL_PROMPT
-          _pry_.pop_prompt
-          _pry_.custom_completions = Pry::DEFAULT_CUSTOM_COMPLETIONS
-          Pry::Commands.delete *Prysql::SQLModeCommandSet.commands.keys
-        else
-          _pry_.push_prompt Prysql::SQL_PROMPT
-          _pry_.custom_completions = proc{ Prysql.interface.completions }
-          Pry::Commands.import Prysql::SQLModeCommandSet
-        end
-      end
-    end
-
-    def query_assignment_proc(var, sql)
-      proc do
-        binding = _pry_.binding_stack.last
-        result = Prysql.interface.execute(sql, :output => false)
-        _pry_.inject_local(var, result, binding)
-      end
     end
 
     def host; interface.host; end
